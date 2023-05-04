38daeb84e9e3559597fd9f32cc2256d866e21308
Howuhh <hsehowuhh@gmail.com>
2023-05-04 22:06:06 +0300

    added generation script

---
+++ scripts/generate.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+export DATA_PATH="../data/nle_data"
+export SAVE_PATH="../data/nle_small_data"
+
+# All allowed role-race-align combos. See also: katakomba/utils/roles.py
+combos=(
+    "arc hum law"
+    "arc hum neu"
+    "arc dwa law"
+    "arc gno neu"
+
+    "bar hum neu"
+    "bar hum cha"
+    "bar orc cha"
+
+    "cav hum law"
+    "cav hum neu"
+    "cav dwa law"
+    "cav gno neu"
+
+    "hea num neu"
+    "hea gno neu"
+
+    "kni num law"
+
+    "mon hum neu"
+    "mon hum law"
+    "mon hum cha"
+
+    "pri hum neu"
+    "pri hum law"
+    "pri hum cha"
+    "pri elf cha"
+
+    "ran hum neu"
+    "ran hum cha"
+    "ran elf cha"
+    "ran gno neu"
+    "ran orc cha"
+
+    "rog hum cha"
+    "rog orc cha"
+
+    "sam hum law"
+
+    "tou hum neu"
+
+    "val hum neu"
+    "val hum law"
+    "val dwa law"
+
+    "wiz hum neu"
+    "wiz hum cha"
+    "wiz elf cha"
+    "wiz gno neu"
+    "wiz orc cha"
+)
+
+for tup in "${combos[@]}"
+do
+    set -- $tup
+    python3 generate_small_dataset.py \
+        --data_path=$DATA_PATH \
+        --save_path=$SAVE_PATH \
+        --role="$1" --race="$2" --alignment="$3" \
+        --num_episodes=350
+done
