a36b293684ca400448a196e503a7f5e63257e9fb
Howuhh <hsehowuhh@gmail.com>
2023-06-14 12:27:51 +0300

    updated requirements and dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM nvidia/cuda:11.7.1-cudnn8-devel-ubuntu20.04
 WORKDIR /workspace
 
-RUN #rm /etc/apt/sources.list.d/cuda.list
-RUN #rm /etc/apt/sources.list.d/nvidia-ml.list
-# python, dependencies for mujoco-py, from https://github.com/openai/mujoco-py
+RUN rm /etc/apt/sources.list.d/cuda.list
+RUN rm /etc/apt/sources.list.d/nvidia-ml.list
+
+# python, dependencies for mujoco-py (and in general useful for RL research),
+# from https://github.com/openai/mujoco-py
 RUN apt-get update -q \
     && DEBIAN_FRONTEND=noninteractive apt-get install -y \
     python3-pip \
@@ -24,21 +26,6 @@ RUN apt-get update -q \
     xserver-xorg-dev \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
-
-# RUN ln -s /usr/bin/python3 /usr/bin/python
-# installing mujoco distr
-RUN mkdir -p /root/.mujoco \
-    && wget https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz -O mujoco.tar.gz \
-    && tar -xf mujoco.tar.gz -C /root/.mujoco \
-    && rm mujoco.tar.gz
-ENV LD_LIBRARY_PATH /root/.mujoco/mujoco210/bin:${LD_LIBRARY_PATH}
-
-# installing dependencies, optional mujoco_py compilation
-COPY requirements.txt requirements.txt
-RUN pip install --pre torch --force-reinstall --index-url https://download.pytorch.org/whl/nightly/cu117
-RUN pip install -r requirements.txt
-
-RUN python3 -c "import mujoco_py"
 
 ### NetHack dependencies
 RUN apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
@@ -65,5 +52,5 @@ RUN apt-get update && apt-get install -yq \
     kitware-archive-keyring
 COPY . /opt/nle
 
-# Install package
-RUN pip install nle
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
--- scripts/stats/small_scale_stats.py
+++ scripts/stats/small_scale_stats.py
@@ -5,14 +5,7 @@ from katakomba.utils.datasets.small_scale import load_nld_aa_small_dataset
 
 
 def main():
-    target_role = "wiz"
-    target_race = "hum"
-    target_align = "cha"
-
     for role, race, align in ALLOWED_COMBOS:
-        # if not (role.value == target_role and race.value == target_race and align.value == target_align):
-        #     continue
-
         df, traj = load_nld_aa_small_dataset(role=role, race=race, align=align, mode="compressed")
         transitions = [t["actions"].shape[0] for t in traj.values()]
         median_score = np.median([df[gameid].attrs["points"] for gameid in list(traj.keys())])
@@ -22,18 +15,16 @@ def main():
         total_transitions = np.sum(transitions)
         total_trajectories = len(traj.keys())
 
-        print(f"{role.value}-{race.value}-{align.value}", median_depth)
+        total_bytes = 0
+        for episode in traj.values():
+            total_bytes += sum([arr.nbytes for arr in episode.values()])
 
-        # total_bytes = 0
-        # for episode in traj.values():
-        #     total_bytes += sum([arr.nbytes for arr in episode.values()])
-        #
-        # total_gb = round(total_bytes / 1e+9, 1)
-        #
-        # print(f"{role.value}-{race.value}-{align.value}", total_gb)
+        total_gb = round(total_bytes / 1e+9, 1)
 
-        # print(f"{role.value}-{race.value}-{align.value}")
-        # print(f"& {total_transitions} & {median_length} & {median_score} & {median_depth} & {total_gb} \\")
+        print(f"{role.value}-{race.value}-{align.value}", total_gb)
+
+        print(f"{role.value}-{race.value}-{align.value}")
+        print(f"& {total_transitions} & {median_length} & {median_score} & {median_depth} & {total_gb} \\")
 
         df.close()
 
